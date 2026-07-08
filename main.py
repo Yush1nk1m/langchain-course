8f073e8c2c991b05629eb32369f56cd4df2c550b
Yush1nk1m <kys010306@sogang.ac.kr>
2026-07-08 22:46:51 +0900

    intro to search agents

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
@@ -7,6 +11,17 @@ from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 load_dotenv()
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="list of sources used to generate the answer")
 
 # tavily = TavilyClient()
 
@@ -24,7 +39,7 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
